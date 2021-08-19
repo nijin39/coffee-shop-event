fbe9d3e46618868a7311ff5d8894cf82d40cb060
Kim Jong IL <jngkim@amazon.com>
2021-08-20 03:54:58 +0900

    config

--- app/test/app.spec.ts
+++
@@ -1,9 +0,0 @@
-import 'mocha';
-import { expect } from 'chai';
-
-describe('App.ts Test suite', function() {
-    it('Application Test Suite', async() => {
-        expect(0).to.equal(0);
-
-    });
-});
